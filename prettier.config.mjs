f5249cc74c73df2796a8eb11d4c0b96242e48d0b
Saurabh-2607 <saurabh.nayla@gmail.com>
2026-04-10 00:03:03 +0530

    chore: add prettier tailwind format checks

---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+import * as prettierPluginTailwindcss from "prettier-plugin-tailwindcss";
+
+/** @type {import("prettier").Config} */
+const prettierConfig = {
+  plugins: [prettierPluginTailwindcss],
+};
+
+export default prettierConfig;
